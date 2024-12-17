2ce47c62cdf6acc581d583c32e3ea0cf7a2ec405
athallahfaathirnugraha <athallahfaathirnugraha@gmail.com>
2024-12-17 22:49:54 +0700

    implement Triangle::point_is_inside

--- src/triangle.rs
+++ src/triangle.rs
@@ -6,3 +6,20 @@ pub struct Triangle {
     pub b: Point,
     pub c: Point,
 }
+
+impl Triangle {
+    pub fn point_is_inside(self, point: Point) -> bool {
+        fn sign(a: Point, b: Point, c: Point) -> f32 {
+            (a.x - c.x) * (b.y - c.y) - (b.x - c.x) * (a.y - c.y)
+        }
+
+        let d1 = sign(point, self.a, self.b);
+        let d2 = sign(point, self.b, self.c);
+        let d3 = sign(point, self.c, self.a);
+
+        let has_neg = (d1 < 0.) || (d2 < 0.) || (d3 < 0.);
+        let has_pos = (d1 > 0.) || (d2 > 0.) || (d3 > 0.);
+
+        !(has_neg && has_pos)
+    }
+}
